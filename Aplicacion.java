d2a1207f2ac0ed8a0906d35987235c4ed10e45ea
CarlosMatellano <carlos.matellano.v@gmail.com>
2024-03-08 12:11:20 +0100

    Update Aplicacion.java

--- Aplicacion.java
+++ Aplicacion.java
@@ -13,7 +13,7 @@ public class Aplicacion {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hola Mundo!");
+        System.out.println("Hola Carlos!");
     }
     
 }
